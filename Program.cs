879bb748113478882e68810a474937a2cb5a22be
Anton <anton_artemjev06@mail.ru>
2022-09-20 22:27:33 +0700

    code

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿string[] a = new string[4];
+
+
+string[] b = new string[4];
+
+int c = 0;
+
+for (int i = 0; i < a.Length; i++)
+ {
+    Console.WriteLine("введите строку");
+    
+    a[i] = Convert.ToString(Console.ReadLine());
+    
+ }
+
+
+for (int i = 0; i < a.Length; i++)
+ {
+    if (a[i].Length <= 3)
+    {
+        b[c] = a[i];
+        c+=1;
+    }
+    
+    
+ }
+Console.Write("[");
+for (int i = 0; i < a.Length; i++)
+ {
+    Console.Write(a[i] + " ");
+ }
+
+Console.Write("] -> [");
+
+for (int i = 0; i < b.Length; i++)
+ {
+    Console.Write(b[i] + " ");
+ }
+
+Console.Write("]");
+
+
+
